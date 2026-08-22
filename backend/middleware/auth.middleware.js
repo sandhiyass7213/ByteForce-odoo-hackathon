bf2cc7c2e2ee1c16aded4517b4fca1f4e0df035c
sandhiyass7213 <sandhiyass7213@gmail.com>
2026-08-22 16:50:10 +0530

    Add files via upload

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+function authenticateToken(req, res, next) {
+
+  const authHeader = req.headers["authorization"];
+
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Access token required"
+    });
+  }
+
+  try {
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET || "dayflow_secret_key"
+    );
+
+    req.user = decoded;
+
+    next();
+
+  } catch (error) {
+
+    return res.status(403).json({
+      success: false,
+      message: "Invalid or expired token"
+    });
+
+  }
+}
+
+module.exports = authenticateToken;
